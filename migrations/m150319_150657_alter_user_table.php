8fd7223ef85428de464ddfcaf4b920fdb21449a4
YeeSoft <makitrataras@gmail.com>
2016-05-21 11:48:25 +0300

    migrations fixed

--- migrations/m150319_150657_alter_user_table.php
+++ migrations/m150319_150657_alter_user_table.php
@@ -8,12 +8,12 @@ class m150319_150657_alter_user_table extends Migration
 
     public function safeUp()
     {
-        $this->addColumn('{{%user}}', 'superadmin', Schema::TYPE_SMALLINT . "(6) DEFAULT '0' AFTER `status`");
-        $this->addColumn('{{%user}}', 'registration_ip', Schema::TYPE_STRING . "(15) DEFAULT NULL AFTER `updated_at`");
-        $this->addColumn('{{%user}}', 'bind_to_ip', Schema::TYPE_STRING . "(255) DEFAULT NULL AFTER `updated_at`");
-        $this->addColumn('{{%user}}', 'email_confirmed', Schema::TYPE_SMALLINT . "(1) NOT NULL DEFAULT '0' AFTER `updated_at`");
-        $this->addColumn('{{%user}}', 'confirmation_token', Schema::TYPE_STRING . "(255) DEFAULT NULL AFTER `updated_at`");
-        $this->addColumn('{{%user}}', 'avatar', Schema::TYPE_TEXT . " DEFAULT NULL AFTER `superadmin`");
+        $this->addColumn('{{%user}}', 'superadmin', $this->integer(6)->defaultValue(0));
+        $this->addColumn('{{%user}}', 'registration_ip', $this->string(15));
+        $this->addColumn('{{%user}}', 'bind_to_ip', $this->string(255));
+        $this->addColumn('{{%user}}', 'email_confirmed', $this->integer(1)->defaultValue(0));
+        $this->addColumn('{{%user}}', 'confirmation_token', $this->string(255));
+        $this->addColumn('{{%user}}', 'avatar', $this->text());
     }
 
     public function safeDown()
