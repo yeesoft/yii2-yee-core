92831dca7b24ebe2b3f8ec4e74fe8528a294cc1a
DimX <dimxdimx@gmail.com>
2016-10-06 13:12:50 +0300

    DimX / ilter not working /
    use parent method

--- grid/CheckboxColumn.php
+++ grid/CheckboxColumn.php
@@ -7,40 +7,6 @@ use Closure;
 
 class CheckboxColumn extends \yii\grid\CheckboxColumn
 {
-
-    /**
-     * Renders the header cell content.
-     * The default implementation simply renders [[header]].
-     * This method may be overridden to customize the rendering of the header cell.
-     * @return string the rendering result
-     */
-    protected function renderHeaderCellContent()
-    {
-        $name = $this->name;
-        if (substr_compare($name, '[]', -2, 2) === 0) {
-            $name = substr($name, 0, -2);
-        }
-        if (substr_compare($name, ']', -1, 1) === 0) {
-            $name = substr($name, 0, -1) . '_all]';
-        } else {
-            $name .= '_all';
-        }
-
-        $id = $this->grid->options['id'];
-        $options = json_encode([
-            'name' => $this->name,
-            'multiple' => $this->multiple,
-            'checkAll' => $name,
-                ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-        $this->grid->getView()->registerJs("jQuery('#$id').yiiGridView('setSelectionColumn', $options);");
-
-        if ($this->header !== null || !$this->multiple) {
-            return parent::renderHeaderCellContent();
-        } else {
-            return Html::checkBox($name, false, ['class' => 'select-on-check-all']);
-        }
-    }
-
     /**
      * @inheritdoc
      */
