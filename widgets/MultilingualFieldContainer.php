2264664c96834afd8ede4b2d6c7436f3c63a9b73
YeeSoft <makitrataras@gmail.com>
2017-03-04 23:03:45 +0200

    fixed yii2-yee-cms#44 bug

--- widgets/MultilingualFieldContainer.php
+++ widgets/MultilingualFieldContainer.php
@@ -3,6 +3,7 @@
 namespace yeesoft\widgets;
 
 use yii\base\Object;
+use yii\helpers\Inflector;
 
 /**
  * MultilinualFieldContainer
@@ -16,9 +17,37 @@ class MultilingualFieldContainer extends Object
     {
         $_html = '';
         foreach ($this->fields as $field) {
-            $_html .= call_user_func_array(array($field, $method), $arguments);
+            $_html .= call_user_func_array(array($field, $method), $this->updateArguments($method, $arguments, $field));
         }
 
         return $_html;
     }
+
+    /**
+     * Fix generating id for multilingual inputs with custom id.
+     * 
+     * @param string $method
+     * @param array $arguments
+     * @param mixed $field
+     * @return array
+     */
+    protected function updateArguments($method, $arguments, $field)
+    {
+        $language = '_' . Inflector::camel2id(Inflector::id2camel($field->language), "_");
+
+        if ($method == 'widget' && isset($arguments[1]['options']['id'])) {
+            $arguments[1]['options']['id'] .= $language;
+        }
+
+        if (in_array($method, ['textInput', 'textarea', 'radio', 'checkbox', 'fileInput', 'hiddenInput', 'passwordInput']) && isset($arguments[0]['id'])) {
+            $arguments[0]['id'] .= $language;
+        }
+
+        if (in_array($method, ['input', 'dropDownList', 'listBox', 'radioList', 'checkboxList']) && isset($arguments[1]['id'])) {
+            $arguments[1]['id'] .= $language;
+        }
+
+        return $arguments;
+    }
+
 }
